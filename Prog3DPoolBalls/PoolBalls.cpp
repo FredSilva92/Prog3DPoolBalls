d4f13ed91b00fef50048d9ec5f8cac6d68a914cf
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2023-06-02 10:42:07 +0100

    Nomes de variáveis globais

--- Prog3DPoolBalls/PoolBalls.cpp
+++ Prog3DPoolBalls/PoolBalls.cpp
@@ -36,35 +36,35 @@ using namespace std;
 #pragma region variáveis globais
 
 // mesa
-const GLuint numberOfTableVertices = 36;
-GLfloat tableVertices[numberOfTableVertices * 8];
-GLuint tableVAO;
+const GLuint _numberOfTableVertices = 36;
+GLfloat _tableVertices[_numberOfTableVertices * 8];
+GLuint _tableVAO;
 GLuint tableVBO;
 
 // bolas
-const GLuint numberOfBalls = 15;
-GLuint ballsVAOs[numberOfBalls];
-GLuint ballsVBOs[numberOfBalls];
-vector<vector<float>> ballsVertices;
+const GLuint _numberOfBalls = 15;
+GLuint _ballsVAOs[_numberOfBalls];
+GLuint _ballsVBOs[_numberOfBalls];
+vector<vector<float>> _ballsVertices;
 
 // shaders
-GLuint programShader;
+GLuint _programShader;
 
 // câmara
-glm::mat4 model, view, projection;
-glm::mat3 normalMatrix;
-GLfloat angle = 0.0f;
-glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 5.0f);
+glm::mat4 _model, _view, _projection;
+glm::mat3 _normalMatrix;
+GLfloat _angle = 0.0f;
+glm::vec3 _cameraPosition = glm::vec3(0.0f, 0.0f, 5.0f);
 
 // eventos do mouse
-float lastX = 0.0f;
-float lastY = 0.0f;
-bool firstMouse = true;
+float _lastX = 0.0f;
+float _lastY = 0.0f;
+bool _firstMouse = true;
 
-float zoomLevel = 1.0f;
-float minZoom = 0.1f;
-float maxZoom = 10.0f;
-float zoomSpeed = 0.1f;
+float _zoomLevel = 1.0f;
+float _minZoom = 0.1f;
+float _maxZoom = 10.0f;
+float _zoomSpeed = 0.1f;
 
 #pragma endregion
 
@@ -82,7 +82,7 @@ void PoolBalls::init(void) {
 	float yTex = 0.0f;
 
 	// cria atributos dos vértices da mesa
-	GLfloat tableVertices[numberOfTableVertices * 8] = {
+	GLfloat _tableVertices[_numberOfTableVertices * 8] = {
 		//*************************************************
 		//                       X+ (face #0)
 		// ************************************************
@@ -153,10 +153,10 @@ void PoolBalls::init(void) {
 	};
 
 	// gera o nome para o VAO da mesa
-	glGenVertexArrays(1, &tableVAO);
+	glGenVertexArrays(1, &_tableVAO);
 
 	// vincula o VAO ao contexto OpenGL atual
-	glBindVertexArray(tableVAO);
+	glBindVertexArray(_tableVAO);
 
 	// gera o nome para o VBO da mesa
 	glGenBuffers(1, &tableVBO);
@@ -165,7 +165,7 @@ void PoolBalls::init(void) {
 	glBindBuffer(GL_ARRAY_BUFFER, tableVBO);
 
 	// inicializa o VBO atualmente ativo com dados imutáveis
-	glBufferStorage(GL_ARRAY_BUFFER, sizeof(tableVertices), tableVertices, 0);
+	glBufferStorage(GL_ARRAY_BUFFER, sizeof(_tableVertices), _tableVertices, 0);
 
 	// ativar atributos das posições dos vértices
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)0);
@@ -183,29 +183,29 @@ void PoolBalls::init(void) {
 	glBindVertexArray(0);
 
 	// carrega os atributos dos vértices de cada bola a partir de ficheiros .obj
-	for (int i = 1; i <= numberOfBalls; i++) {
+	for (int i = 1; i <= _numberOfBalls; i++) {
 		string filename = "textures/Ball" + to_string(i) + ".obj";
-		ballsVertices.push_back(PoolBalls::loadTextures(filename.c_str()));
+		_ballsVertices.push_back(PoolBalls::loadTextures(filename.c_str()));
 	}
 
 	// gera nomes para os VAOs das bolas
-	glGenVertexArrays(numberOfBalls, ballsVAOs);
+	glGenVertexArrays(_numberOfBalls, _ballsVAOs);
 
 	// vincula cada VAO das bolas ao contexto OpenGL atual
-	for (int i = 0; i < numberOfBalls; i++) {
-		glBindVertexArray(ballsVAOs[i]);
+	for (int i = 0; i < _numberOfBalls; i++) {
+		glBindVertexArray(_ballsVAOs[i]);
 	}
 
 	// gera nomes para os VBOs das bolas
-	glGenBuffers(numberOfBalls, ballsVBOs);
+	glGenBuffers(_numberOfBalls, _ballsVBOs);
 
 	// cria e configurar cada VBO das bolas
-	for (int i = 0; i < ballsVertices.size(); i++) {
+	for (int i = 0; i < _ballsVertices.size(); i++) {
 		// vincula o VBO ao contexto OpenGL atual
-		glBindBuffer(GL_ARRAY_BUFFER, ballsVBOs[i]);
+		glBindBuffer(GL_ARRAY_BUFFER, _ballsVBOs[i]);
 
 		// inicializa o vbo atualmente ativo com dados imutáveis
-		glBufferStorage(GL_ARRAY_BUFFER, ballsVertices[i].size() * sizeof(float), ballsVertices[i].data(), 0);
+		glBufferStorage(GL_ARRAY_BUFFER, _ballsVertices[i].size() * sizeof(float), _ballsVertices[i].data(), 0);
 
 		// ativar atributos das posições dos vértices
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)0);
@@ -231,21 +231,21 @@ void PoolBalls::init(void) {
 	};
 
 	// carrega shaders
-	programShader = loadShaders(shaders);
+	_programShader = loadShaders(shaders);
 
 	// se houve erros ao carregar shaders
-	if (!programShader) {
+	if (!_programShader) {
 		cout << "Erro ao carregar shaders: " << endl;
 		exit(EXIT_FAILURE);
 	}
 
 	// vincula o programa shader ao contexto OpenGL atual
-	glUseProgram(programShader);
+	glUseProgram(_programShader);
 
 	// obtém as localizações dos atributos no programa shader
-	GLint positionId = glGetProgramResourceLocation(programShader, GL_PROGRAM_INPUT, "position");
-	GLint normalId = glGetProgramResourceLocation(programShader, GL_PROGRAM_INPUT, "normal");
-	GLint textCoordId = glGetProgramResourceLocation(programShader, GL_PROGRAM_INPUT, "textCoord");
+	GLint positionId = glGetProgramResourceLocation(_programShader, GL_PROGRAM_INPUT, "position");
+	GLint normalId = glGetProgramResourceLocation(_programShader, GL_PROGRAM_INPUT, "normal");
+	GLint textCoordId = glGetProgramResourceLocation(_programShader, GL_PROGRAM_INPUT, "textCoord");
 
 	// faz a ligação entre os atributos do programa shader ao VAO e VBO ativos 
 	glVertexAttribPointer(positionId, 3 /*3 elementos por vértice*/, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -258,29 +258,29 @@ void PoolBalls::init(void) {
 	glEnableVertexAttribArray(textCoordId);
 
 	// matrizes de transformação
-	model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
-	view = glm::lookAt(
-		cameraPosition,					// eye (posição da câmara).
+	_model = glm::rotate(glm::mat4(1.0f), _angle, glm::vec3(0.0f, 1.0f, 0.0f));
+	_view = glm::lookAt(
+		_cameraPosition,					// eye (posição da câmara).
 		glm::vec3(0.0f, 0.0f, 0.0f),	// center (para onde está a "olhar")
 		glm::vec3(0.0f, 1.0f, 0.0f)		// up
 	);
-	glm::mat4 modelView = view * model;
-	projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-	normalMatrix = glm::inverseTranspose(glm::mat3(modelView));
+	glm::mat4 modelView = _view * _model;
+	_projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	_normalMatrix = glm::inverseTranspose(glm::mat3(modelView));
 
 	// obtém as localizações dos uniforms no programa shader
-	GLint modelId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "Model");
-	GLint viewId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "View");
-	GLint modelViewId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "ModelView");
-	GLint projectionId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "Projection");
-	GLint normalViewId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "NormalMatrix");
+	GLint modelId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "Model");
+	GLint viewId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "View");
+	GLint modelViewId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "ModelView");
+	GLint projectionId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "Projection");
+	GLint normalViewId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "NormalMatrix");
 
 	// atribui o valor aos uniforms do programa shader
-	glProgramUniformMatrix4fv(programShader, modelId, 1, GL_FALSE, glm::value_ptr(model));
-	glProgramUniformMatrix4fv(programShader, viewId, 1, GL_FALSE, glm::value_ptr(view));
-	glProgramUniformMatrix4fv(programShader, modelViewId, 1, GL_FALSE, glm::value_ptr(modelView));
-	glProgramUniformMatrix4fv(programShader, projectionId, 1, GL_FALSE, glm::value_ptr(projection));
-	glProgramUniformMatrix3fv(programShader, normalViewId, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+	glProgramUniformMatrix4fv(_programShader, modelId, 1, GL_FALSE, glm::value_ptr(_model));
+	glProgramUniformMatrix4fv(_programShader, viewId, 1, GL_FALSE, glm::value_ptr(_view));
+	glProgramUniformMatrix4fv(_programShader, modelViewId, 1, GL_FALSE, glm::value_ptr(modelView));
+	glProgramUniformMatrix4fv(_programShader, projectionId, 1, GL_FALSE, glm::value_ptr(_projection));
+	glProgramUniformMatrix3fv(_programShader, normalViewId, 1, GL_FALSE, glm::value_ptr(_normalMatrix));
 
 	// define a janela de renderização
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -294,37 +294,38 @@ void PoolBalls::display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// translação da mesa
-	glm::mat4 translatedModel = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+	glm::mat4 translatedModel = glm::translate(_model, glm::vec3(0.5f, 0.0f, 0.0f));
 
 	// modelo de visualização do objeto
-	glm::mat4 modelView = view * translatedModel;
+	glm::mat4 modelView = _view * translatedModel;
 
 	// obtém a localização do uniform
-	GLint modelViewId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "ModelView");
+	GLint modelViewId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "ModelView");
 
 	// atribui o valor ao uniform
-	glProgramUniformMatrix4fv(programShader, modelViewId, 1, GL_FALSE, glm::value_ptr(modelView));
+	glProgramUniformMatrix4fv(_programShader, modelViewId, 1, GL_FALSE, glm::value_ptr(modelView));
 
 	// desenha a mesa na tela
-	glBindVertexArray(tableVAO);
-	glDrawArrays(GL_TRIANGLES, 0, numberOfTableVertices);
+	glBindVertexArray(_tableVAO);
+	glDrawArrays(GL_TRIANGLES, 0, _numberOfTableVertices);
 
-	// desenha cada bola na tela
-	for (int i = 0; i < ballsVertices.size(); i++) {
+	// para cada bola
+	for (int i = 0; i < _ballsVertices.size(); i++) {
 		// translação da mesa
-		glm::mat4 translatedModel = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		glm::mat4 translatedModel = glm::translate(_model, glm::vec3(0.0f, 0.0f, 0.0f));
 
 		// modelo de visualização do objeto
-		glm::mat4 modelView = view * translatedModel;
+		glm::mat4 modelView = _view * translatedModel;
 
 		// obtém a localização do uniform
-		GLint modelViewId = glGetProgramResourceLocation(programShader, GL_UNIFORM, "ModelView");
+		GLint modelViewId = glGetProgramResourceLocation(_programShader, GL_UNIFORM, "ModelView");
 
 		// atribui o valor ao uniform
-		glProgramUniformMatrix4fv(programShader, modelViewId, 1, GL_FALSE, glm::value_ptr(modelView));
+		glProgramUniformMatrix4fv(_programShader, modelViewId, 1, GL_FALSE, glm::value_ptr(modelView));
 
-		glBindVertexArray(ballsVAOs[i]);
-		glDrawArrays(GL_POINTS, 0, ballsVertices[i].size() / 8);
+		// desenha a bola na tela
+		glBindVertexArray(_ballsVAOs[i]);
+		glDrawArrays(GL_POINTS, 0, _ballsVertices[i].size() / 8);
 	}
 }
 
@@ -392,47 +393,47 @@ void PoolBalls::scrollCallback(GLFWwindow* window, double xoffset, double yoffse
 	glfwGetWindowSize(window, &width, &height);
 
 	// Calculate the zoom factor based on the scroll offset
-	float zoomFactor = 1.0f + static_cast<float>(yoffset) * zoomSpeed;
+	float zoomFactor = 1.0f + static_cast<float>(yoffset) * _zoomSpeed;
 
-	view = glm::scale(view, glm::vec3(zoomFactor, zoomFactor, 1.0f));
+	_view = glm::scale(_view, glm::vec3(zoomFactor, zoomFactor, 1.0f));
 
-	zoomLevel += yoffset * zoomSpeed;
-	zoomLevel = std::max(minZoom, std::min(maxZoom, zoomLevel));
+	_zoomLevel += yoffset * _zoomSpeed;
+	_zoomLevel = std::max(_minZoom, std::min(_maxZoom, _zoomLevel));
 }
 
 void PoolBalls::mouseCallback(GLFWwindow* window, double xpos, double ypos)
 {
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) != GLFW_PRESS)
 	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
+		_lastX = xpos;
+		_lastY = ypos;
+		_firstMouse = false;
 		return; // Sai da função se o botão do mouse esquerdo não estiver pressionado
 	}
 
-	if (firstMouse)
+	if (_firstMouse)
 	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
+		_lastX = xpos;
+		_lastY = ypos;
+		_firstMouse = false;
 	}
 
-	float xOffset = xpos - lastX;
-	float yOffset = ypos - lastY;
+	float xOffset = xpos - _lastX;
+	float yOffset = ypos - _lastY;
 
-	lastX = xpos;
-	lastY = ypos;
+	_lastX = xpos;
+	_lastY = ypos;
 
 	float sensitivity = 0.1f;
 
 	// Aplicar rotação horizontal 
-	view = glm::rotate(view, glm::radians(xOffset), glm::vec3(0.0f, 1.0f, 0.0f));
+	_view = glm::rotate(_view, glm::radians(xOffset), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	// Calcular o vetor direito da câmera
-	glm::vec3 right = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(view[2])));
+	glm::vec3 right = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(_view[2])));
 
 	// Aplicar rotação vertical 
-	view = glm::rotate(view, glm::radians(yOffset), right);
+	_view = glm::rotate(_view, glm::radians(yOffset), right);
 }
 
 void PoolBalls::charCallback(GLFWwindow* window, unsigned int codepoint)
